bef3fce5173f475f23a09233c2d0e9510852cbc6
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-04-14 12:57:03 +0300

    singleResponsibility

--- solidPrinciples/src/singleResponsibilityPrinciple/SingleResponsibility.java
+++ solidPrinciples/src/singleResponsibilityPrinciple/SingleResponsibility.java
@@ -1,7 +1,11 @@
 package singleResponsibilityPrinciple;
 
+import singleResponsibilityPrinciple.withSRP.employeeMS.EmployeeDAO;
+import singleResponsibilityPrinciple.withSRP.employeeMS.Employee;
+
 public class SingleResponsibility {
     public static void main(String args[]){
-        System.out.println("hello world");
+        Employee employee=new Employee(1,"mohammad","it",true);
+        EmployeeDAO.saveEmployeeToDatabase(employee);
     }
 }
